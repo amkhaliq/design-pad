0de4567044bf0fbf3a38a498d8f188aa1a59feb3
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-11-01 03:33:11 -0500

    added grid layout for board design

--- src/components/DesignBoard.js
+++ src/components/DesignBoard.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react'
 import './index.css'
+import PadInput from './PadInput'
 
 class DesignBoard extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            containers: []
+        }
+    }
+
+    allowDrop = (event) => {
+        event.preventDefault();
+    }
+
+    onDrop = (event) => {
+        event.preventDefault();
+        let data = event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(data));
     }
 
     render () {
         return (
-            <div className="design-cntr">
-
+            /** 2x2 grid */
+            <div className="board-container">
+                <div>
+                    <PadInput />
+                </div>
+                    <div className="design-board">  
+                        <div className="board-input"></div>
+                        <div className="board-input"></div>
+                        <div className="board-input"></div>
+                        <div className="board-input"></div>
+                    </div>
             </div>
         );
     }
