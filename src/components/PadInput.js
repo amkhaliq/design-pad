91c051a9034b83d09b491198f9fac61a6a8378cc
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-11-01 03:30:05 -0500

    added drag functionality and grid layout pad input

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -3,11 +3,22 @@ import React, {Component} from 'react'
 import './index.css'
 
 class PadInput extends Component {
+    onDrag = (event) => {
+        event.dataTransfer.setData("text", event.target.id);
+    }
 
     render() {
         return (
-            <div>
-
+            /** grid-layout */
+            <div className="pad-input">
+                <div id="c-red" draggable="true" onDragStart={this.onDrag}>1</div>
+                <div id="c-blue" draggable="true" onDragStart={this.onDrag}>2</div>
+                <div id="c-green" draggable="true" onDragStart={this.onDrag}>3</div>
+                <div id="c-black" draggable="true" onDragStart={this.onDrag}>4</div>
+                <div id="c-orange" draggable="true" onDragStart={this.onDrag}>5</div>
+                <div id="c-yellow" draggable="true" onDragStart={this.onDrag}>6</div>
+                <div id="c-cyan" draggable="true" onDragStart={this.onDrag}>7</div>
+                <div id="c-brown" draggable="true" onDragStart={this.onDrag}>8</div>
             </div>
         );
     }
