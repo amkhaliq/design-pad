80e714c3ff2afd199139f7044d61eaea15d94006
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-04 03:42:29 -0500

    added pad input and drag functionality

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -3,22 +3,52 @@ import React, {Component} from 'react'
 import './index.css'
 
 class PadInput extends Component {
-    onDrag = (event) => {
-        event.dataTransfer.setData("text", event.target.id);
+    
+
+    onDrag(event) {
+        const color = event.target.style.backgroundColor;
+
+        event.dataTransfer.setData("text", color)
     }
 
     render() {
+
         return (
-            /** grid-layout */
+            /** grid-layout 
+             *  Draggable elements within the PadInput component
+            */
             <div className="pad-input">
-                <div id="c-red" draggable="true" onDragStart={this.onDrag}>1</div>
-                <div id="c-blue" draggable="true" onDragStart={this.onDrag}>2</div>
-                <div id="c-green" draggable="true" onDragStart={this.onDrag}>3</div>
-                <div id="c-black" draggable="true" onDragStart={this.onDrag}>4</div>
-                <div id="c-orange" draggable="true" onDragStart={this.onDrag}>5</div>
-                <div id="c-yellow" draggable="true" onDragStart={this.onDrag}>6</div>
-                <div id="c-cyan" draggable="true" onDragStart={this.onDrag}>7</div>
-                <div id="c-brown" draggable="true" onDragStart={this.onDrag}>8</div>
+                <div id="item-1" style={{backgroundColor: "#DC143C"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>1</div>
+
+                <div id="item-2" style={{backgroundColor: "#191970"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>2</div>
+
+                <div id="item-3" style={{backgroundColor: "#008080"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>3</div>
+
+                <div id="item-4" style={{backgroundColor: "#000000"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>4</div>
+
+                <div id="item-5" style={{backgroundColor: "#FFA500"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>5</div>
+
+                <div id="item-6" style={{backgroundColor: "#FFD700"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>6</div>
+
+                <div id="item-7" style={{backgroundColor: "#00FFFF"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>7</div>
+
+                <div id="item-8" style={{backgroundColor: "#D2691E"}} 
+                                draggable={true} 
+                                onDragStart={this.onDrag}>8</div>
             </div>
         );
     }
