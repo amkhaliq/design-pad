aa8273157c2fa3f23f9b76d2a56a54ca0c328f12
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-06 19:46:19 -0500

    deleted files and refactored code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './index.css';
 
 function App() {
   return (
+    /**
+     * Code snippet below will render the webpage
+     */
     <div className="container">
       <h1 id="title">DESIGN PAD</h1>
-      <div className="app-title">
-        
-        <DesignContainer />
-
-      </div>
+        <div className="content-align">
+          <DesignContainer />
+        </div>
     </div>
   );
 }
